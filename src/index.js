adadbb6ece0dc4ec3da44ffce0a146c08081188d
jana-droidcon <154016511+jana-droidcon@users.noreply.github.com>
2024-01-19 10:39:19 +0100

    injected automated icon

--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formateDate(date);
   conditionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
+
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formateDate(date) {
